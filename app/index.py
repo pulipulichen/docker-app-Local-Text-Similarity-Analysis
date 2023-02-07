5550e72687a3059711e857c52651cfa0625bbbf1
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 02:19:27 +0800

    Wed Feb  8 02:19:27 AM CST 2023

--- app/index.py
+++ app/index.py
@@ -1,5 +1,10 @@
 # print('hello world')
 
 import os
+from pathlib import Path
+
 entries = os.listdir('input/')
-print(entries)
+for entry in entries:
+  print(entry)
+  file_stats = os.stat('input/' + entry)
+  print(file_stats.st_size)
