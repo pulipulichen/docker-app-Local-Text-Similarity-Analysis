16f970829def3d03a2724d4a79a2dad20ae30a40
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 15:14:32 +0800

    Wed Feb  8 03:14:32 PM CST 2023

--- app/lib_python/AppendFulltext.py
+++ app/lib_python/AppendFulltext.py
@@ -4,20 +4,24 @@ import os
 # import parser object from tike
 from tika import parser  
 
+from cachetools import cached, LRUCache
+
+@cached(cache=LRUCache(maxsize=100))
+def extractContentFromFile(file):
+  parsed_file = parser.from_file(file)
+  data = parsed_file['content'] 
+
+  data = data.replace('\n', ' ')
+  while data.find('  ') != -1:
+    data = data.replace('  ', ' ')
+    
+  return data
+
 def AppendFulltext(files):
   # 加上檔案大小
   for i, file in enumerate(files):
-    parsed_file = parser.from_file(file['file'])
-      
-    # saving content of pdf
-    # you can also bring text only, by parsed_pdf['text'] 
-    # parsed_pdf['content'] returns string 
-    data = parsed_file['content'] 
-
-    data = data.replace('\n', ' ')
-    while data.find('  ') != -1:
-      data = data.replace('  ', ' ')
-      
+    data = extractContentFromFile(file['file'])
+    
     # Printing of content 
     files[i]['data_length'] = len(data)
     files[i]['data'] = data
