2d0d66a190bc5ea8dc8aafa27e8d3b8c000bd971
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 18:11:54 +0800

    Wed Feb  8 06:11:54 PM CST 2023

--- app/lib_python/Save/SaveCYJS.py
+++ app/lib_python/Save/SaveCYJS.py
@@ -4,19 +4,19 @@ import json
 def SaveCYJS(G, filename):
   cyjs = nx.cytoscape_data(G)
 
-  pos = nx.spring_layout(G)
-  nodes = cyjs['elements']['nodes']
-  for node in nodes:
-    data = node['data']
-    id = data['id']
+  # pos = nx.spring_layout(G)
+  # nodes = cyjs['elements']['nodes']
+  # for node in nodes:
+  #   data = node['data']
+  #   id = data['id']
 
-    node["position"] = {}
-    node["position"]["x"] = pos[id][0]
-    node["position"]["y"] = pos[id][1]
-  cyjs['elements']['nodes'] = nodes
+  #   node["position"] = {}
+  #   node["position"]["x"] = pos[id][0]
+  #   node["position"]["y"] = pos[id][1]
+  # cyjs['elements']['nodes'] = nodes
 
   cyjs = json.dumps(cyjs, ensure_ascii=False)
 
-  text_file = open('input/' + filename + ".json", "w", encoding='utf-8')
+  text_file = open('input/' + filename + ".cyjs", "w", encoding='utf-8')
   n = text_file.write(cyjs)
   text_file.close()
