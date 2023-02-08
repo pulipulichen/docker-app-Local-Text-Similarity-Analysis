b3dd9b1015bb409ab45e96cb9252704148d72b1d
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 11:54:38 +0800

    Wed Feb  8 11:54:38 AM CST 2023

--- app/lib_python/ExtractZip.py
+++ app/lib_python/ExtractZip.py
@@ -4,9 +4,9 @@ from zipfile import ZipFile
 import subprocess
 
 def ExtractZip():
-  subprocess.call(['mv', '/cache/.gitignore', '/tmp'])
-  subprocess.call(['rm', '-rf', '/cache/*'])
-  subprocess.call(['mv', '/tmp/.gitignore', '/cache/'])
+  print(subprocess.call(['mv', '/cache/.gitignore', '/tmp']))
+  print(subprocess.call(['rm', '-rf', '/cache/*']))
+  print(subprocess.call(['mv', '/tmp/.gitignore', '/cache/']))
 
   entries = os.listdir('input/')
   for entry in entries:
