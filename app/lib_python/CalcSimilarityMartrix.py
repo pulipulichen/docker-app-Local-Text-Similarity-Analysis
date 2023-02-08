5f88f7c0112ebf6bbb791c71b3ea598ee7d9cde5
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 12:36:35 +0800

    Wed Feb  8 12:36:35 PM CST 2023

--- app/lib_python/CalcSimilarityMartrix.py
+++ app/lib_python/CalcSimilarityMartrix.py
@@ -1,6 +1,11 @@
+from itertools import combinations
 from fastDamerauLevenshtein import damerauLevenshtein
 
 def CalcSimilarityMartrix(files):
+  martix = []
+
+  print(list(combinations(range(0, len(files)),2)))
+
   distance = damerauLevenshtein('ca', 'abc', similarity=False)
   print(distance)
   return True
