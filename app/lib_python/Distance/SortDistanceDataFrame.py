2f6ae2ea514b88fb0f1a0d9374137ee4d44f3839
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 19:33:30 +0800

    Wed Feb  8 07:33:30 PM CST 2023

--- app/lib_python/Distance/SortDistanceDataFrame.py
+++ app/lib_python/Distance/SortDistanceDataFrame.py
@@ -1,4 +1,4 @@
 
 def SortDistanceDataFrame(files):
-  files = files.sort_values(by=['is_outlier', 'cluster'], ascending=False)
+  files = files.sort_values(by=['is_outlier', 'cluster', 'modified_time'], ascending=False)
   return files
