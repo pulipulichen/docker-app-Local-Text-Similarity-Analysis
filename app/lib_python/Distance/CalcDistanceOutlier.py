4630a4a494ea62ae1720f1fe0902e50d50ce27f6
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 13:57:08 +0800

    Wed Feb  8 01:57:08 PM CST 2023

--- app/lib_python/Distance/CalcDistanceOutlier.py
+++ app/lib_python/Distance/CalcDistanceOutlier.py
@@ -1,10 +1,17 @@
+import numpy as np
 
-def find_outliers_IQR(df):
+def CalcDistanceOutlier(df):
+  # print(len(find_outliers_IQR(df['value'])))
+
+  values = df['value']
   q1=df.quantile(0.25)
   q3=df.quantile(0.75)
   IQR=q3-q1
-  outliers = df[((df<(q1-1.5*IQR)) | (df>(q3+1.5*IQR)))]
-  return outliers
+  down_bound = q1-1.5*IQR
+  up_bound = q3+1.5*IQR
 
-def CalcDistanceOutlier(df):
-  print(len(find_outliers_IQR(df['value'])))
+  df['is_outlier'] = np.where((df['value'] < down_bound | df['value'] > up_bound), True, False)
+
+  print(df)
+
+  return df
