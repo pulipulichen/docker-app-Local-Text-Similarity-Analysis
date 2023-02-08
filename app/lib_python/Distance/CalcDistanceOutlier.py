ff6b6dd1a9cb5b0004ad2089a4aedcf730b2dc4b
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 14:01:57 +0800

    Wed Feb  8 02:01:57 PM CST 2023

--- app/lib_python/Distance/CalcDistanceOutlier.py
+++ app/lib_python/Distance/CalcDistanceOutlier.py
@@ -7,10 +7,10 @@ def CalcDistanceOutlier(df):
   q1 = values.quantile(0.25)
   q3 = values.quantile(0.75)
   IQR = q3 - q1
-  down_bound = q1 - 1.5 * IQR
+  down_bound = int(q1 - 1.5 * IQR)
   up_bound = q3 + 1.5 * IQR
 
-  print(df[df['value'] < down_bound | df['value'] > up_bound])
+  print(df[df['value'] < down_bound])
 
   print(df)
 
