fbe00bf962e018271ed6967bb1242fb032ce5ebc
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 14:32:34 +0800

    Wed Feb  8 02:32:34 PM CST 2023

--- app/lib_python/Distance/ClusteringFIles.py
+++ app/lib_python/Distance/ClusteringFIles.py
@@ -9,14 +9,15 @@ def ClusteringFIles(df):
   # G = nx.barbell_graph(5, 1)
   G = nx.Graph()
   graph_df = df[['source', 'target', 'value']]
+  graph_df['value'] = 1/graph_df['value']
   elist = list(graph_df.itertuples(index=False))
   G.add_weighted_edges_from(elist)
 
-  # communities_generator = community.girvan_newman(G)
-  # top_level_communities = next(communities_generator)
-  # next_level_communities = next(communities_generator)
-  # output = sorted(map(sorted, next_level_communities))
-  # print(output)
-
-  output = community.louvain_communities(G, seed=123)
+  communities_generator = community.girvan_newman(G)
+  top_level_communities = next(communities_generator)
+  next_level_communities = next(communities_generator)
+  output = sorted(map(sorted, next_level_communities))
   print(output)
+
+  # output = community.louvain_communities(G, weight='weight', seed=123)
+  # print(output)
