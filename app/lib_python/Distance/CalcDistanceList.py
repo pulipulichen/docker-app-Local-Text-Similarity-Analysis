137a1fb7659d90e80e87f75bb7d63e1f99386887
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 13:10:43 +0800

    Wed Feb  8 01:10:43 PM CST 2023

--- app/lib_python/Distance/CalcDistanceList.py
+++ app/lib_python/Distance/CalcDistanceList.py
@@ -11,11 +11,11 @@ def CalcDistanceList(files):
     source = files[pair[0]]
     target = files[pair[1]]
     distance = damerauLevenshtein(source['data'], target['data'], similarity=False)
-    df = df.append({
+    df = pd.concat([df, {
       "source": source["user"],
       "target": target["user"],
       "value": distance
-    }, ignore_index = True)
+    }])
 
   print(df)
   return df
