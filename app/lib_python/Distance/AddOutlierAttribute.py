5a47e09d20d052b4d882ddf1c427d54e53ba7609
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 17:39:44 +0800

    ok

--- app/lib_python/Distance/AddOutlierAttribute.py
+++ app/lib_python/Distance/AddOutlierAttribute.py
@@ -3,6 +3,12 @@ import networkx as nx
 def AddOutlierAttribute(G, outliers):
   nodes = G.nodes
   print(nodes)
+
+  outliers_dict = {}
+  for node in nodes:
+    outliers_dict[node] = node.isin(outliers)
+
+  print(outliers_dict)
   # for outlier in outliers:
   #   nx.set_node_attributes(G, (), name="is_outlier")
   return G
