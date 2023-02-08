8864f4fee3e44d185137e979a4d6f30dcfb98914
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 17:08:50 +0800

    ok

--- app/lib_python/Save/SaveCYJS.py
+++ app/lib_python/Save/SaveCYJS.py
@@ -3,7 +3,7 @@ import json
 
 def SaveCYJS(G, filename):
   cyjs = nx.cytoscape_data(G)
-  cyjs = json.dumps(cyjs, ensure_ascii=False, encoding='utf-8')
+  cyjs = json.dumps(cyjs, ensure_ascii=False)
 
   text_file = open('input/' + filename + ".cyjs", "w", encoding='utf-8')
   n = text_file.write(cyjs)
