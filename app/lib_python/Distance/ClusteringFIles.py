615441ec926b88261480e80ddb3be76e5cd9e925
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 14:27:40 +0800

    Wed Feb  8 02:27:40 PM CST 2023

--- app/lib_python/Distance/ClusteringFIles.py
+++ app/lib_python/Distance/ClusteringFIles.py
@@ -5,7 +5,12 @@ from .GetOutliers import *
 def ClusteringFIles(df):
 
   outliers = GetOutliers(df)
-  G = nx.barbell_graph(5, 1)
+  
+  # G = nx.barbell_graph(5, 1)
+  G = nx.Graph()
+  elist = list(df.itertuples(index=False))
+  G.add_weighted_edges_from(elist)
+
   communities_generator = community.girvan_newman(G)
   top_level_communities = next(communities_generator)
   next_level_communities = next(communities_generator)
