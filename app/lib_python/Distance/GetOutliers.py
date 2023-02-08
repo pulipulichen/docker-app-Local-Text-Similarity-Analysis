b7aca2b47606d4bb0c6dd31f9da0593e726cd064
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 14:12:37 +0800

    Wed Feb  8 02:12:36 PM CST 2023

--- app/lib_python/Distance/GetOutliers.py
+++ app/lib_python/Distance/GetOutliers.py
@@ -1,10 +1,11 @@
 def GetOutliers(df):
   df = df[df['is_outlier'] == True]
 
-  outliers = []
+  
   sources = df['source'].tolist()
   targets = df['target'].tolist()
-  outliers = sources.concat(targets)
+
+  outliers = sources + targets
   outliers = list(set(outliers))
 
   print(outliers)
