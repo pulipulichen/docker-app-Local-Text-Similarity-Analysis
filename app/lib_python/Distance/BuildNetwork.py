f1777c9d9c47aa91a6dce00c0e1a330aed502992
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 17:51:02 +0800

    ok

--- app/lib_python/Distance/BuildNetwork.py
+++ app/lib_python/Distance/BuildNetwork.py
@@ -2,12 +2,14 @@ import networkx as nx
 
 def BuildNetwork(df):
   G = nx.Graph()
-  
+
   df['value_norm'] = (df['value']-df['value'].min())/(df['value'].max()-df['value'].min())
   df['weight'] = 1 - df['value_norm']
 
   graph_df = df[['source', 'target', 'weight']]
   elist = list(graph_df.itertuples(index=False))
   G.add_weighted_edges_from(elist)
+
+  nx.spring_layout(G)
 
   return G
