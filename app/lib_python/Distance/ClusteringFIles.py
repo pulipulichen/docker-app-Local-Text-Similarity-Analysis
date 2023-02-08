60148e203d93fd1aa3d4ce70c39572617d4e0aac
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 14:52:06 +0800

    Wed Feb  8 02:52:06 PM CST 2023

--- app/lib_python/Distance/ClusteringFIles.py
+++ app/lib_python/Distance/ClusteringFIles.py
@@ -11,10 +11,10 @@ def ClusteringFIles(df):
   
   # G = nx.barbell_graph(5, 1)
   G = nx.Graph()
-  df['weight'] = 1/ df['value']
-  df['weight_norm'] = (df['weight']-df['weight'].min())/(df['weight'].max()-df['weight'].min())
+  df['value_norm'] = (df['value']-df['value'].min())/(df['value'].max()-df['value'].min())
+  df['weight'] = 1 - df['value_norm']
 
-  graph_df = df[['source', 'target', 'weight_norm']]
+  graph_df = df[['source', 'target', 'weight']]
   elist = list(graph_df.itertuples(index=False))
   G.add_weighted_edges_from(elist)
   
