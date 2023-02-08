f4e15c6f8c3e6147854adb725ca8daaac5c1feeb
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 18:31:49 +0800

    Wed Feb  8 06:31:49 PM CST 2023

--- app/lib_python/ExtractZip.py
+++ app/lib_python/ExtractZip.py
@@ -22,12 +22,12 @@ def ExtractZip():
     filename = os.path.splitext(entry)[0]
     print(filename)
     if os.path.isdir('cache/files/' + filename):
-      print('yes')
-      print(subprocess.run(['ls', './cache/files/'], capture_output=True))
+      # print('yes')
+      # print(subprocess.run(['ls', './cache/files/'], capture_output=True))
       # subprocess.run(['mv', './cache/files/' + filename + '/*', './cache/files/'], capture_output=True)
       file_names = os.listdir('./cache/files/' + filename)
       for file_name in file_names:
         shutil.move(os.path.join('./cache/files/' + filename, file_name), './cache/files/')
-      # subprocess.run(['rm', '-rf', './cache/files/' + filename], capture_output=True)
-      print(subprocess.run(['ls', './cache/files/'], capture_output=True))
+      subprocess.run(['rm', '-rf', './cache/files/' + filename], capture_output=True)
+      # print(subprocess.run(['ls', './cache/files/'], capture_output=True))
     return filename
