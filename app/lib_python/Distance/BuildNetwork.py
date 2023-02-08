3087e67f7ff4eeed9ee4a9c9d355160147e137a1
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 18:02:38 +0800

    ok

--- app/lib_python/Distance/BuildNetwork.py
+++ app/lib_python/Distance/BuildNetwork.py
@@ -10,10 +10,10 @@ def BuildNetwork(df):
   elist = list(graph_df.itertuples(index=False))
   G.add_weighted_edges_from(elist)
 
-  pos = nx.spring_layout(G)
-  # print(pos)
-  # nx.set_node_attributes(G, pos, name="pos")
-  for n, p in enumerate(pos):
-    G.nodes[n]['pos'] = p
+  # pos = nx.spring_layout(G)
+  # # print(pos)
+  # # nx.set_node_attributes(G, pos, name="pos")
+  # for n, p in enumerate(pos):
+  #   G.nodes[n]['pos'] = p
 
   return G
--- app/lib_python/Save/SaveCYJS.py
+++ app/lib_python/Save/SaveCYJS.py
@@ -3,6 +3,12 @@ import json
 
 def SaveCYJS(G, filename):
   cyjs = nx.cytoscape_data(G)
+
+  pos = nx.spring_layout(G)
+  with cyjs.elements.nodes as nodes:
+    print(nodes)
+
+
   cyjs = json.dumps(cyjs, ensure_ascii=False)
 
   text_file = open('input/' + filename + ".json", "w", encoding='utf-8')
