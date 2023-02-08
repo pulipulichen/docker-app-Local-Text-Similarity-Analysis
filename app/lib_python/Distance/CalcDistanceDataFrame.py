760647f7087326aa610dc6a8cb4e15202038b9b9
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 16:04:10 +0800

    ok

--- app/lib_python/Distance/CalcDistanceDataFrame.py
+++ app/lib_python/Distance/CalcDistanceDataFrame.py
@@ -10,8 +10,8 @@ def CalcDistanceDataFrame(files):
   pairs = list(combinations(range(0, len(files)),2))
   for pair in pairs:
     # print(str(pair[0]) + '-' + str(pair[1]))
-    source = files[pair[0]]
-    target = files[pair[1]]
+    source = files.iloc[[pair[0]]]
+    target = files.iloc[[pair[1]]]
     distance = damerauLevenshtein(source['data'], target['data'], similarity=False)
     record = {
       "source": source["user"],
