6689b83b94212ba0169613b0ab120931fa8deda0
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 12:39:25 +0800

    Wed Feb  8 12:39:25 PM CST 2023

--- app/lib_python/CalcSimilarityMartrix.py
+++ app/lib_python/CalcSimilarityMartrix.py
@@ -6,6 +6,7 @@ def CalcSimilarityMartrix(files):
 
   pairs = list(combinations(range(0, len(files)),2))
   for pair in pairs:
+    pair = list(pair)
     print(pair[0] + '-' + pair[1])
   
   distance = damerauLevenshtein('ca', 'abc', similarity=False)
