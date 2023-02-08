8dfef5bb91902f9bb8c9e9e61f8dab9c4b0b1b75
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 14:41:51 +0800

    Wed Feb  8 02:41:51 PM CST 2023

--- app/lib_python/Distance/ClusteringFIles.py
+++ app/lib_python/Distance/ClusteringFIles.py
@@ -12,7 +12,7 @@ def ClusteringFIles(df):
   graph_df = df[['source', 'target', 'weight']]
   elist = list(graph_df.itertuples(index=False))
   G.add_weighted_edges_from(elist)
-  print(nx.to_pandas_edgelist(G))
+  print(nx.to_pandas_edgelist(G).set_option('display.max_columns', None))
 
   # communities_generator = community.girvan_newman(G)
   # top_level_communities = next(communities_generator)
@@ -24,6 +24,6 @@ def ClusteringFIles(df):
   # print(output)
 
   G = nx.petersen_graph()
-  print(nx.to_pandas_edgelist(G))
+  print(nx.to_pandas_edgelist(G).set_option('display.max_columns', None))
   output = community.louvain_communities(G, seed=123)
   print(output)
