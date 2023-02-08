183dba803c09aa73816a51b11c9dfcb9939d38e3
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 18:39:59 +0800

    ok

--- app/lib_python/Files/AppendFulltext.py
+++ app/lib_python/Files/AppendFulltext.py
@@ -22,11 +22,16 @@ def extractContentFromFile(file):
   sql_cache.set(file, data)
   return data
 
-def getContentLength(data):
-  return len(data)
+# def getContentLength(data):
+#   return len(data)
+
+def getContentFooter(data):
+  return data[len(data) - 100:]
+
 
 def AppendFulltext(files):
   files['data'] = files['file'].apply(extractContentFromFile)
   files['data_length'] = files['data'].apply(len)
+  files['data_end'] = files['data'].apply(getContentFooter)
 
   return files
