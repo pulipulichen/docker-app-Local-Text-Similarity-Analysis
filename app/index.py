17c59b3a810a1ceae183eb395d072ea24d0c88ea
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 10:22:15 +0800

    Wed Feb  8 10:22:15 AM CST 2023

--- app/index.py
+++ app/index.py
@@ -5,6 +5,10 @@ from pathlib import Path
 
 from zipfile import ZipFile
 
+import subprocess
+
+subprocess.call(['rm', '-rf', 'rm !(\.gitignore)'])
+
 entries = os.listdir('input/')
 for entry in entries:
   if entry.endswith('.zip') == False:
@@ -13,3 +17,22 @@ for entry in entries:
   with ZipFile('input/' + entry, 'r') as zObject:
     zObject.extractall(
         path='cache/')
+
+entries = os.listdir('cache/')
+files = {}
+for entry in entries:
+  if os.path.isdir('cache/' + entry) == False:
+    continue
+
+  subentries = os.listdir('cache/' + entry)
+  max_time = 0
+  last_entry = None
+  for subentry in subentries:
+    time_m = os.path.getmtime('cache/' + entry + '/' + subentry)
+    if (time_m > max_time):
+      last_entry = 'cache/' + entry + '/' + subentry
+  
+  if last_entry is not None:
+    files[entry] = last_entry
+ 
+print(files)
