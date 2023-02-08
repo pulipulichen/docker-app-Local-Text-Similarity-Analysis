dc3a9325d45847c5c3493d04ce139e7199d54bf7
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 18:31:16 +0800

    Wed Feb  8 06:31:16 PM CST 2023

--- app/lib_python/ExtractZip.py
+++ app/lib_python/ExtractZip.py
@@ -23,11 +23,11 @@ def ExtractZip():
     print(filename)
     if os.path.isdir('cache/files/' + filename):
       print('yes')
-      print(subprocess.run(['ls', './cache/files/'], capture_output=True).wait())
-      # subprocess.run(['mv', './cache/files/' + filename + '/*', './cache/files/'], capture_output=True).wait()
+      print(subprocess.run(['ls', './cache/files/'], capture_output=True))
+      # subprocess.run(['mv', './cache/files/' + filename + '/*', './cache/files/'], capture_output=True)
       file_names = os.listdir('./cache/files/' + filename)
       for file_name in file_names:
         shutil.move(os.path.join('./cache/files/' + filename, file_name), './cache/files/')
-      # subprocess.run(['rm', '-rf', './cache/files/' + filename], capture_output=True).wait()
-      print(subprocess.run(['ls', './cache/files/'], capture_output=True).wait())
+      # subprocess.run(['rm', '-rf', './cache/files/' + filename], capture_output=True)
+      print(subprocess.run(['ls', './cache/files/'], capture_output=True))
     return filename
