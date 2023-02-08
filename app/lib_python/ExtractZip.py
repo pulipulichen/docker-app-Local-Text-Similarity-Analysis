e2d3df6ee0335855528dd59be4c82573f8bf705d
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-02-08 12:21:26 +0800

    Wed Feb  8 12:21:26 PM CST 2023

--- app/lib_python/ExtractZip.py
+++ app/lib_python/ExtractZip.py
@@ -5,8 +5,8 @@ import subprocess
 
 def ExtractZip():
   print(subprocess.run(['mv', '/cache/.gitignore', '/tmp'], capture_output=True))
-  print(subprocess.call(['rm', '-rf', '/cache/*'], capture_output=True))
-  print(subprocess.call(['mv', '/tmp/.gitignore', '/cache/'], capture_output=True))
+  print(subprocess.run(['rm', '-rf', '/cache/*'], capture_output=True))
+  print(subprocess.run(['mv', '/tmp/.gitignore', '/cache/'], capture_output=True))
 
   return False
   entries = os.listdir('input/')
